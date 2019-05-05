0b037dba73500d1988ed3ddeb4613992613490cf
Michael Hathaway <michael@hathaways.me.uk>
2019-05-05 19:12:18 +0100

    State transition changed Should roll over days smoothly now, day wrapping accounted for

--- state.py
+++ state.py
@@ -1,10 +1,11 @@
+import time
 class State(object):
     next_state = None
     colour = 0x000000
     light_on = True
-    def __init__(self, start, end, start_col, end_col, name):
+    def __init__(self, start, next_s, start_col, end_col, name):
         self.start = start
-        self.end = end
+        self.next_state = next_s
         self.start_col = start_col
         self.end_col = end_col
         self.name = name
@@ -13,10 +14,11 @@ class State(object):
     Returns true if time t is valid for state
     """
     def is_valid(self, t):
-        return self.end.time_cmp(t) == -1 and not self.start.time_cmp(t) == -1
-    
-    def next_state(self):
-        return self.child_state
+        if self.next_state.start.tm_mday == 0 and self.start.tm_mday == 1:
+            b_time = time.struct_time((0, 0, 0, self.start.tm_hour, self.start.tm_min, 0, 1, -1, -1))
+        else:
+            b_time = self.start
+        return self.next_state.start > t and not b_time > t
 
     def enter(self, light_on):
         self.light_on = light_on
@@ -26,4 +28,4 @@ class State(object):
             return self.start_col
         else:
             return 0x000000
-        # work out ratio and write to LEDs
+        # work out ratio and write to LEDs 
